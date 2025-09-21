bf3ebbddc0bdffc28ab06f9bf36e555d8139427a
Lance <tech14@sumomedia.co>
2025-09-21 10:29:30 +0800

    Add confirmation alert modal to logout.

--- project/assets/js/logout.js
+++ project/assets/js/logout.js
@@ -1,25 +1,36 @@
 
 jQuery(function($){
     $(document).on('click', '.logout, #logout', function() {
-        $.ajax({
-            url: './controller/LogoutController.php',
-            type: 'POST',
-            dataType: 'json',
-            success: function(response) {
-                if(response.success) {
-                    Swal.fire({
-                        title: 'Success',
-                        text: response.message,
-                        icon: 'success',
-                        showConfirmButton: false
-                    });
-                    setTimeout(function(){
-                        window.location.href = response.redirect;
-                    }, 1000)
-                }
-            },
-            error: function(xhr, status, error) {
-                console.error(error);
+        Swal.fire({
+            title: "Are you sure, you want to logout?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: './controller/LogoutController.php',
+                    type: 'POST',
+                    dataType: 'json',
+                    success: function(response) {
+                        if(response.success) {
+                            Swal.fire({
+                                title: 'Success',
+                                text: response.message,
+                                icon: 'success',
+                                showConfirmButton: false
+                            });
+                            setTimeout(function(){
+                                window.location.href = response.redirect;
+                            }, 1000)
+                        }
+                    },
+                    error: function(xhr, status, error) {
+                        console.error(error);
+                    }
+                });
             }
         });
     })
