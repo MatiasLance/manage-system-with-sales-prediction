608d51029872a72eee8b2335a840bf267742fb8d
Lance <tech14@sumomedia.co>
2025-10-03 13:14:21 +0800

    Updated user table.

--- database/migration/migrate.php
+++ database/migration/migrate.php
@@ -23,7 +23,7 @@ class DatabaseMigrator
                 lastname VARCHAR(50) NOT NULL,
                 email VARCHAR(100) UNIQUE NOT NULL,
                 password VARCHAR(255) NOT NULL,
-                user_type ENUM('admin', 'user') NOT NULL,
+                user_type ENUM('admin', 'cashier', 'manager') NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )",
